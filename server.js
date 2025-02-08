c1736f8d76d264735bd4c1fb052e59bd72c3944c
SteamSite <newsteamsite@gmail.com>
2025-02-08 16:52:45 +0200

    GFOKGFKDOGJ

--- server.js
+++ server.js
@@ -27,9 +27,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "Увійти.html"));
 });
 
-
-
-
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
